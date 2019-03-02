2a824aeeb5fc56141f83bbb0b1435d2534e16d1b
kitasuke <yusuke2759@gmail.com>
2019-03-02 23:25:45 +0900

    Add SwiftSyntax

--- Package.swift
+++ Package.swift
@@ -7,14 +7,14 @@ let package = Package(
     name: "SwiftOptimizer",
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-syntax.git", .exact("0.40200.0")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SwiftOptimizer",
-            dependencies: []),
+            dependencies: ["SwiftSyntax"]),
         .testTarget(
             name: "SwiftOptimizerTests",
             dependencies: ["SwiftOptimizer"]),
