33a83b3c42a67245d0b698a1987f7952ccb008f0
kitasuke <yusuke2759@gmail.com>
2019-04-11 11:35:18 +0900

    Update version

--- Sources/IBOutletRewriter/Commands/Version.swift
+++ Sources/IBOutletRewriter/Commands/Version.swift
@@ -17,7 +17,7 @@ struct VersionCommand: CommandProtocol {
     let function = "Display current version of IBOutletRewriter"
     
     func run(_ options: Options) -> Result<(), AnyError> {
-        print("0.1.0")
+        print("0.2.0")
         return .init(value: ())
     }
 }
